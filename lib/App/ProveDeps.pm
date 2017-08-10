3ac1e3d61c756b11ef226c049793e8de82cb2480
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-08-10 16:47:06 +0700

    Hackety hack

--- lib/App/ProveDeps.pm
+++ lib/App/ProveDeps.pm
@@ -8,7 +8,98 @@ use strict;
 use warnings;
 use Log::ger;
 
+use File::chdir;
+
 our %SPEC;
+
+sub _find_dist_dir {
+    my ($dist, $dirs) = @_;
+
+  DIR:
+    for my $dir (@$dirs) {
+        my @entries = do {
+            opendir my $dh, $dir or do {
+                warn "prove-deps: Can't opendir '$dir': $!\n";
+                next DIR;
+            };
+            my @entries = grep { $_ ne '.' && $_ ne '..' } readdir $dh;
+            closedir $dh;
+            @entries;
+        };
+        #log_trace("entries: %s", \@entries);
+      FIND:
+        {
+            my @res;
+
+            # exact match
+            @res = grep { $_ eq $dist } @entries;
+            #log_trace("exact matches: %s", \@res);
+            return "$dir/$res[0]" if @res == 1;
+
+            # case-insensitive match
+            my $dist_lc = lc $dist;
+            @res = grep { lc($_) eq $dist_lc } @entries;
+            return "$dir/$res[0]" if @res == 1;
+
+            # suffix match, e.g. perl-DIST or cpan_DIST
+            @res = grep { /\A\w+[_-]\Q$dist\E\z/ } @entries;
+            #log_trace("suffix matches: %s", \@res);
+            return "$dir/$res[0]" if @res == 1;
+
+            # prefix match, e.g. DIST-perl
+            @res = grep { /\A\Q$dist\E[_-]\w+\z/ } @entries;
+            return "$dir/$res[0]" if @res == 1;
+        }
+    }
+    undef;
+}
+
+sub _prove {
+    require IPC::System::Options;
+
+    my $opts = shift;
+
+    my $stdout = "";
+    my $stderr = "";
+
+    my $act_stdout;
+    my $act_stderr;
+    if (log_is_warn()) {
+        $act_stdout = "tee_stdout";
+        $act_stderr = "tee_stderr";
+    } else {
+        $act_stdout = "capture_stdout";
+        $act_stderr = "capture_stderr";
+    }
+    IPC::System::Options::system(
+        {
+            log=>1,
+            ($act_stdout => \$stdout) x !!$act_stdout,
+            ($act_stderr => \$stderr) x !!$act_stderr,
+        },
+        "prove", @{ $opts || [] },
+        log_is_debug() ? ("-v") : (),
+    );
+    if ($?) {
+        if ($stdout =~ /^Result: FAIL/m) {
+            my $detail = "";
+            if ($stdout =~ m!^(Failed \d+/\d+ subtests|No subtests run)!m) {
+                $detail = " ($1)";
+            }
+            [500, "Test failed". $detail];
+        } else {
+            [500, "Non-zero exit code (".($? >> 8).")"];
+        }
+    } else {
+        if ($stdout =~ /^Result: PASS/m) {
+            [200, "PASS"];
+        } elsif ($stdout =~ /^Result: NOTESTS/m) {
+            [200, "NOTESTS"];
+        } else {
+            [500, "No PASS marker"];
+        }
+    }
+}
 
 $SPEC{prove_deps} = {
     v => 1.1,
@@ -47,24 +138,49 @@ _
         dist_dirs => {
             summary => 'Where to find the distributions',
             'x.name.is_plural' => 1,
-            'x.name.singular' => 'dir',
+            'x.name.singular' => 'dist_dir',
             schema => ['array*', of=>'dirname*'],
             req => 1,
         },
-        # XXX level, currently direct dependents only
+
         phases => {
             summary => 'Only select dists that depend in these phases',
             'x.name.is_plural' => 1,
             'x.name.singular' => 'phase',
             schema => ['array*', of=>'str*'],
+            tags => ['category:filtering'],
         },
         rels => {
             summary => 'Only select dists that depend using these relationships',
             'x.name.is_plural' => 1,
             'x.name.singular' => 'rel',
             schema => ['array*', of=>'str*'],
+            tags => ['category:filtering'],
         },
-        # XXX dzil test instead of prove
+
+        exclude_dists => {
+            'x.name.is_plural' => 1,
+            'x.name.singular' => 'rel',
+            schema => ['array*', of=>'perl::distname*'],
+            tags => ['category:filtering'],
+        },
+        include_dists => {
+            'x.name.is_plural' => 1,
+            'x.name.singular' => 'rel',
+            schema => ['array*', of=>'perl::distname*'],
+            tags => ['category:filtering'],
+        },
+        exclude_dist_pattern => {
+            schema => 're*',
+            tags => ['category:filtering'],
+        },
+        include_dist_pattern => {
+            schema => 're*',
+            tags => ['category:filtering'],
+        },
+
+        # XXX add arg: level, currently direct dependents only
+        # XXX add arg: dzil test instead of prove
     },
     deps => {
         prog => 'prove',
@@ -85,17 +201,54 @@ sub prove_deps {
     return [500, "Can't lcpan rdeps: $res->[0] - $res->[1]"]
         unless $res->[0] == 200;
 
+    my @fails;
+    my $i = 0;
   REC:
     for my $rec (@{ $res->[2] }) {
+        $i++;
         if (defined $args{phases} && @{ $args{phases} }) {
             next REC unless grep $rec->{phase} eq $_, @{ $args{phases} };
         }
         if (defined $args{rel} && @{ $args{rel} }) {
             next REC unless grep $rec->{rel} eq $_, @{ $args{rel} };
         }
-        log_info "Found dep: %s", $rec->{dist};
+        log_info "Found dep: %s (%s %s)", $rec->{dist}, $rec->{phase}, $rec->{rel};
+
+        my $dir = _find_dist_dir($rec->{dist}, $args{dist_dirs});
+        unless (defined $dir) {
+            log_error "Can't find dir for dist '%s', skipped", $rec->{dist};
+            push @fails, {dist=>$rec->{dist}, reason=>"Can't find dist dir"};
+            next REC;
+        }
+
+        if ($args{-dry_run}) {
+            log_info("[DRY] [%d/%d] Running prove for dist '%s' in '%s' ...",
+                     $i, scalar(@{ $res->[2] }),
+                     $rec->{dist}, $dir);
+            next REC;
+        }
+
+        {
+            local $CWD = $dir;
+            log_warn("[%d/%d] Running prove for dist '%s' in '%s' ...",
+                     $i, scalar(@{ $res->[2] }),
+                     $rec->{dist}, $dir);
+            my $pres = _prove($args{prove_opts});
+            log_debug("Prove result: %s", $pres);
+            if ($pres->[0] == 200) {
+                # success
+            } else {
+                log_error "Test for dist '%s' failed: %s",
+                    $rec->{dist}, $pres->[1];
+                push @fails, {dist=>$rec->{dist}, reason=>$pres->[1]};
+            }
+        }
     }
-    [200];
+
+    [
+        @{@fails == 0 ? [200, "All succeeded"] : @fails == @{$res} ? [500, "All failed"] : [200, "Some failed"]},
+        \@fails
+    ];
 }
 
 1;
